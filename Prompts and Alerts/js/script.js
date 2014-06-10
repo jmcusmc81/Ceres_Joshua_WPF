7e1ebb65b815878da44c391861d18357c45e3bf8
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-10 14:19:21 -0400

    added var that takes user input and calculates width times height and prints it out on console,

--- Prompts and Alerts/js/script.js
+++ Prompts and Alerts/js/script.js
@@ -6,3 +6,5 @@ console.log(userInput);
 
 var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");
 var height = prompt("Please enter the height");
+var area = width * height; //calculates with that info
+console.log(area); //prints it out to console
