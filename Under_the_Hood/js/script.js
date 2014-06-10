6a197a3ad8db2293c2839ae29f10de6cd2956c54
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-10 11:14:09 -0400

    Booleans and variables

--- Under_the_Hood/js/script.js
+++ Under_the_Hood/js/script.js
@@ -15,3 +15,6 @@ var age; //declaration
 age=32; //definition
 //storage comtainer = value
 var yearOfBirth = 1981; //declaring and defining on the same line.
+//Booleans
+var isStudent = false;
+var firstName = "Kermit";
