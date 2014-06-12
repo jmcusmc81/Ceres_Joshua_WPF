2f9707f298afd8930915802f3c7db7985a75e414
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-12 10:40:54 -0400

    Trying out provided example

--- Expression_worksheet/js/script.js
+++ Expression_worksheet/js/script.js
@@ -1,3 +1,30 @@
+//Calculate the Circumference of a Circle
+
+var radius = 6;
+const PI = 3.14159265;
+
+//multiply 2, PI and the radius & assign to circ variable.
+var circ = radius * 2 * PI;
+//print out the output circumference
+console.log(circ);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Created by Josh on 6/12/14.
  */
