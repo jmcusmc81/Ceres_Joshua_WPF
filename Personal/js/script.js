9a8c4f85fe8e949c7cef5e93774239eb7bc53607
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-12 14:58:08 -0400

    Setting up the math for the personal prompt message

--- Personal/js/script.js
+++ Personal/js/script.js
@@ -1,3 +1,18 @@
+var diapers = prompt("How many diapers do you change per day? \nPlease enter the quantity");
+var daysYear = 365; //How man calendar days are in a year
+var a=365
+var diapersNeeded = diapers * a; //calculates with that info
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Created by Josh on 6/12/14.
  */
