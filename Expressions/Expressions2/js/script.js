896201a300779b917c2d33bc118c3e82ebecf177
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-12 09:32:33 -0400

    //Expressions - Concatenating Strings

--- Expressions/Expressions2/js/script.js
+++ Expressions/Expressions2/js/script.js
@@ -1,10 +1,17 @@
 //Expressions - Expressions with Arrays
-
+/*
 var orangeBins=[234,567,883];
 var total=orangeBins[0]+orangeBins[1]+orangeBins[2];
 console.log(total);
+    */
 
+//Expressions - Concatenating Strings
 
+var firstName="Kermit";
+var lastName="The Frog";
+var fullName=firstName+" "+lastName;
+
+console.log(fullName);
 /**
  * Created by Josh on 6/12/14.
  */
