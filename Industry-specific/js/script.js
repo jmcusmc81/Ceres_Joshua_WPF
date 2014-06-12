efd36b3d0b7341f3fa6a34d6bc62e9ebffcba79b
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-12 19:24:00 -0400

    Updated Comments and ran console log to check formulas.

--- Industry-specific/js/script.js
+++ Industry-specific/js/script.js
@@ -7,14 +7,16 @@
 //These Law's and algebraic formulas help me in my job daily.
 
 //Problem: I have a 480V Motor with 100 Amps Current(I). I need to figure out the Resistance(R) and the Power(W).
+//Units of measurement are Volts = Voltage, Amps = Current, Ohms = Resistance, Watts = Power.
 
 var voltage= 480;
 var amps=100;
 var amps=100;
-var resistance=voltage/amps;
+var resistance=voltage/amps + " Ohms";
 console.log(resistance);
-var power=voltage*amps;
+var power=voltage*amps + " Watts";
 console.log(power);
+
 
 
 
