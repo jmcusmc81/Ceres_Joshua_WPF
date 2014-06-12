58e0a222df879193267883a795aa94040ab81ffa
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-12 09:27:42 -0400

    Expressions with Arrays

---
+++ Expressions/Expressions2/js/script.js
@@ -0,0 +1,10 @@
+//Expressions - Expressions with Arrays
+
+var orangeBins=[234,567,883];
+var total=orangeBins[0]+orangeBins[1]+orangeBins[2];
+console.log(total);
+
+
+/**
+ * Created by Josh on 6/12/14.
+ */
--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -36,5 +36,10 @@ var quiz4=80;
 
 //the average is the sum of the items divided by the number of the items
 
-var average=quiz1+quiz2+quiz3+quiz4/4;
-console.log(average);
+var average=(quiz1+quiz2+quiz3+quiz4)/4;
+//console.log(average);
+
+var length=7;
+var width=6;
+var perimeter=length*2+width*2;
+console.log(perimeter);
