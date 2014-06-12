29b533455662e0565aa2daa03a3d2eb4e32101bf
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-12 09:11:30 -0400

    //Expressions - PEMDAS - The Order of Operations

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -22,6 +22,19 @@ console.log(area);
     */
 
 //Expression - Modulo Operator
-
+/*
 var remainder = 32%10;
 console.log(remainder);
+    */
+
+//Expressions - PEMDAS - The Order of Operations
+
+var quiz1=87;
+var quiz2=100;
+var quiz3=60;
+var quiz4=80;
+
+//the average is the sum of the items divided by the number of the items
+
+var average=quiz1+quiz2+quiz3+quiz4;
+console.log(average);
