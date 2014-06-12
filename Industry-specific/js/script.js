a725fde3c68bc6fdc6444fa21ed9500e20c0e81a
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-12 19:10:16 -0400

    added voltage variable

--- Industry-specific/js/script.js
+++ Industry-specific/js/script.js
@@ -1,4 +1,14 @@
+// Joshua Ceres
+// June 12, 2014
+//Industry-specific
 
+
+//I have worked as an Aviation and Production Electronics and Instrumentation Technician for most my adult career. I use a lot of Algebra Ohm's Law and Boolean's Law.
+//These Law's and algebraic formulas help me in my job daily.
+
+//Problem: I have a 480V Motor with 100 Amps Current(I). I need to figure out the Resistance(R) and the Power(W).
+
+var voltage= 480;
 
 
 
@@ -11,5 +21,5 @@
 
 
 /**
- * Created by Josh on 6/12/14.
+ * Created by Josh Ceres Industry Specific on 6/12/14.
  */
