c0a1601a6bc445c3025439d06677a33929d9ffc6
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-12 07:33:50 -0400

    year of birth and current year to show age in console log

--- Expressions/js/script.js
+++ Expressions/js/script.js
@@ -1,7 +1,16 @@
 //Expressions
+
+/*
 var a = 2; //sets up our variable a and defines it with 2
 a = a + 3; //add to a
 console.log(a)
+*/
+
+//Expressions - Age Example
+
+var yearBorn = 1974;
+var age = 2012 - yearBorn;
+console.log(age);
 /**
  * Created by Josh on 6/10/14.
  */
