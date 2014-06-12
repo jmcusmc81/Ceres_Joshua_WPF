90da268fc302e5c91f009ca049a12d627b2110cb
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-12 19:09:00 -0400

    added voltage value

--- Personal/js/script.js
+++ Personal/js/script.js
@@ -1,7 +1,7 @@
 //Joshua Ceres
 //June 12, 2014
 //Personal
-
+//I chose diapers because I go through this everyday and can relate to this the most. Its time consuming and costs a lot of money.
 
 var diapers = prompt("How many diapers do you change per day? \nPlease enter the quantity");
 console.log(diapers);
@@ -12,6 +12,10 @@ console.log(quantity);
 var result= "The amount of diapers you will need is " + quantity + " per year";
 alert(result);
 console.log(result);
+
+//Prompt makes user enter how many diapers per day he/she uses. Then my formula takes that and times it by how many days are in a calendar year.
+//Then it sends the result back in the form of an alert so the user knows what he/she needs. This is a bare minimun based off how many they change per day.
+//There can always be extra because of mishaps with a baby.
 
 
 
