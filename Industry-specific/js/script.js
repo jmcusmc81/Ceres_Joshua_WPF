6495fbb0bb8e1dab5f60a7c99e1462788c9387d8
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-12 19:24:18 -0400

    added watts and ohmms variables

--- Industry-specific/js/script.js
+++ Industry-specific/js/script.js
@@ -7,7 +7,7 @@
 //These Law's and algebraic formulas help me in my job daily.
 
 //Problem: I have a 480V Motor with 100 Amps Current(I). I need to figure out the Resistance(R) and the Power(W).
-//Units of measurement are Volts = Voltage, Amps = Current, Ohms = Resistance, Watts = Power.
+//Units of measurement are Volts (E) = Voltage, Amps(I) = Current, Ohms(R) = Resistance, Watts(P) = Power.
 
 var voltage= 480;
 var amps=100;
@@ -16,6 +16,12 @@ var resistance=voltage/amps + " Ohms";
 console.log(resistance);
 var power=voltage*amps + " Watts";
 console.log(power);
+
+//Now I am going to revers the Problem and make sure I get the same results for Voltage and Current.
+
+var watts= 48000
+var ohmms= 4.8
+var volts=
 
 
 
