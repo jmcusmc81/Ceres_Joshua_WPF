335c696afd05df305b9ff266c1527eb2f91a7e76
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-12 21:58:53 -0400

    Showing total rage in an array format

--- Wacky/js/script.js
+++ Wacky/js/script.js
@@ -36,6 +36,10 @@ console.log(tenthHour);
 var fourteenthHour = c + c;//This time of night she may be worn out. Its still a 50/50 chance though.
 
 
+var totalRage = ["Second", "Third", "Fifth", "Tenth"];//0,1,2,3
+console.log(totalRage);
+
+
 
 
 
