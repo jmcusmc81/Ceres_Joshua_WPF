71eb0ff15a8f28cf32eee1a85f4bdcd521a02e7b
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-12 21:14:12 -0400

    Rage variables being added up hour by hour until maximum of 10 is reached.

--- Wacky/js/script.js
+++ Wacky/js/script.js
@@ -1,16 +1,20 @@
 //This is my Wacky project. I am going to assign number values to incidents that make my wife angry. This is going to be a scale of 1-10. Once she reaches 10 then she loses it.
 //My wife is half filipino-half italian so she can do a 180 degree turn at any time and just unleash hell in the house. I sweat I think she is bi-polar but she wont get check out.
-
+//The goal of this is to track how many times my wife goes full on rage mode.
 var a = 5; //Messy House
 var b = 4; //Kids crying
 var c = 3.5; //Repeats herself. This usually takes three times of her repeating herself before she loses it.
-var d = 3; //Kids do not do chores
+var d = 3; //Kids do not do chores or argue
 var e = 2.5; //I forget to do something that she asked me to do. I am slowly wearing her out on this issue.
 //Once this adds up to 10 watch out!!
 console.log(rage);
 var rageExplosion = 10; //We have reached 10 now run for cover!
-//First hour of the day 7:00 A.M.
-var firstHour =;
+//First hour of the day 7:00 - 8:00 A.M.
+var firstHour = d;
+//Second hour 8:00 - 9:00 A.M.
+var secondHour = d + c + b + " Rage at Maximum!";// Rage at Maximum and well on our way to a Nuclear Meltdown.
+console.log(secondHour);
+alert(secondHour);
 
 
 
