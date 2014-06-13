b5d007b8e8e067baba389b17117a9b7694ddd8bd
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-12 20:54:19 -0400

    added variables with values to show the point scale for my wifes rage indicators.

---
+++ Wacky/js/script.js
@@ -0,0 +1,15 @@
+//This is my Wacky project. I am going to assign number values to incidents that make my wife angry. This is going to be a scale of 1-10. Once she reaches 10 then she loses it.
+//My wife is half filipino-half italian so she can do a 180 degree turn at any time and just unleash hell in the house. I sweat I think she is bi-polar but she wont get check out.
+
+var a = 5; //Messy House
+var b = 4; //Kids crying
+var c = 3.5; //Repeats herself. This usually takes three times of her repeating herself before she loses it.
+var d = 3; //Kids do not do chores
+var e = 2.5; //I forget to do something that she asked me to do. I am slowly wearing her out on this issue.
+
+
+
+
+/**
+ * Created by Josh on 6/12/14.
+ */
