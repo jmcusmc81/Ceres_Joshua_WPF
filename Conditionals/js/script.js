7ebb72a70a2543bd07739123add68245acb8b54c
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-16 09:13:32 -0400

    following along wiht instructor

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -39,6 +39,11 @@ var budget=300;
 var  iPhonePrice=199.99;
 var paycheck=200;
 
+if(iPhonePrice<budget){
+    console.log("We can buy the phone!");
+}else{
+    console.log("No phone for you!!");
+}
 
 //if the price of our phone is less then our budget
 /**
