c644381a29f4d4d93e7d3747cec25c73bc6d03fc
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-16 09:44:41 -0400

    wonLoterry variable added and changed statement to or(||)

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -37,9 +37,9 @@ if(kidHeight>minHeight){
 
 var budget=300;
 var  iPhonePrice=199.99;
-var paycheck=200;
+var wonLottery=true;
 
-if(iPhonePrice<budget && paycheck>300){
+if(iPhonePrice<budget || wonLottery ===true){
    console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!!");
