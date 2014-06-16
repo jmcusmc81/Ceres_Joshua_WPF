b971be2eb04768de7845bf4a170ebaef2d4d0b01
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-16 08:58:48 -0400

    changed kidHeight to test out the else if code

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -14,8 +14,9 @@ if(sunny){
  */
 //Conditional Logic-else if
 
-var kidHeight=52;
+var kidHeight=46;
 var minHeight=48;
+var wParentHeight=45;
 //var sneakerLift=2;
 
 //if the child is old enough print to the console
@@ -23,15 +24,12 @@ var minHeight=48;
 if(kidHeight>minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
-}else if(){
-    //code performed if condition is false
-}
-
-
-
+}else if(kidHeight>wParentHeight){
+    //you can ride with a parent present
+    console.log("You can ride, but only with a parent present.");
 }else{
     //sorry kid you have growing to do
-    console.log("Sorry kid you got some growing to do");
+    console.log("Sorry kid you got some growing to do!");
 }
 /**
  * Created by Josh on 6/16/14.
