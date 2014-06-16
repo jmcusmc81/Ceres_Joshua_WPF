647c87f853de63baef8fc5450a75d8b57dbb869b
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-16 08:42:48 -0400

    COnditional Logic if and else

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -16,16 +16,19 @@ if(sunny){
 
 var kidHeight=47;
 var minHeight=48;
-var sneakerLift=2;
+//var sneakerLift=2;
 
 //if the child is old enough print to the console
 //if the kid height is over 48 inches in height
-if(kidHeight+sneakerLift>minHeight){
+if(kidHeight>minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
 }
 
+if(kidHeight<minHeight){
+    console.log("Sorry you got some growing to do");
 
+}
 /**
  * Created by Josh on 6/16/14.
  */
