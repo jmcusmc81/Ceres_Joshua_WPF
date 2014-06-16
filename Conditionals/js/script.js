2194439023ce8617068d90fc7a8fc18878d89b47
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-16 08:39:55 -0400

    added var sneakerLift

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -16,10 +16,11 @@ if(sunny){
 
 var kidHeight=47;
 var minHeight=48;
+var sneakerLift=2;
 
 //if the child is old enough print to the console
 //if the kid height is over 48 inches in height
-if(kidHeight+2>minHeight){
+if(kidHeight+sneakerLift>minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
 }
