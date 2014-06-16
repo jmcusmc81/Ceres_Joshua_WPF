1ff973e33070844452ed2884fcf1a5341b4242cf
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-16 10:13:06 -0400

    Writing out a Ternary line for a if else statement

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -57,12 +57,23 @@ if(paycheck>300){
 
 
 //Logical Operators - Ternary Operators
+/*
 if(condition){
     do if true;
 }else{
     do if false;
 }
+    */
 
+var gpa =48;
+//if the gpa is over the min 2.0 score, the student can graduate
+/*if(gpa>2.0){
+    console.log("You can graduate!");
+}else{
+    console.log("GPA is too low!");
+}*/
+
+(gpa>2.0) ? console.log("You can graduate!") : console.log("GPA is too low!");
 
 /**
  * Created by Josh on 6/16/14.
