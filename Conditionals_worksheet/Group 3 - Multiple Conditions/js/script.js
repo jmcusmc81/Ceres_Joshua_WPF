acf83efd6bdc92ff21920c3d86cec61e430533ae
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-18 13:15:26 -0400

    Group 3 question about movie ticket price

--- Conditionals_worksheet/Group 3 - Multiple Conditions/js/script.js
+++ Conditionals_worksheet/Group 3 - Multiple Conditions/js/script.js
@@ -6,3 +6,15 @@
 //Conditional worksheet Group 3
 
 //Group 3 Multiple Conditions
+
+//Movie Ticket Price
+
+//The local movie theater in town has a ticket price of $12.00 but if you are a senior (55 and older), under 10 you get the discounted price of $7.00.
+//In addition, if a customer is seeing a movie between 3pm and 5pm they can also get the discounted price.
+//Determine which of the two prices the customer is eligible for.
+
+//Given: age is 40
+//Time of Movie (Assume whole numbers here)
+//Age of the customer
+//Result To Print Out:
+//“The ticket price is X”
