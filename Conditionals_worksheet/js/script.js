7d877d14bbf01cf68dec53cce43d541062eb064a
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-18 08:13:28 -0400

    First question condtional with expressions

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -2,6 +2,15 @@
 //June 16 2014
 //Conditional Worksheet
 
+
+//Celsius to Fahrenheit converter
+
+//Convert a temperature to either degrees Celsius or degrees Fahrenheit depending on what the user has entered.
+//If the user puts a “C” for the unit, the calculator should convert to Celsius.
+//If the user puts “F” for the unit, the calculator should convert to Fahrenheit.
+
+
+
 /**
  * Created by Josh on 6/16/14.
  */
