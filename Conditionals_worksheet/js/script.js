30405519ee60425735dd79a99ffb1e272ef54cdb
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-18 09:38:57 -0400

    COuld not figure out how to put in the informoation for celsius and fahrenheit

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -9,13 +9,18 @@
 //If the user puts a “C” for the unit, the calculator should convert to Celsius.
 //If the user puts “F” for the unit, the calculator should convert to Fahrenheit.
 
-//Formula for Celsius to Fahrenheit (°C × 9/5) + 32 = °F
-//Formula for Fahrenheit to Celsius (°F - 32) x 5/9 = °C
+//Formula for Celsius to Fahrenheit (°C × 9/5) + 32 = °F or °C × 1.8 + 32 = °F
+//Formula for Fahrenheit to Celsius (°F - 32) x 5/9 = °C or (°F - 32) x 1.8 = °C
+/*
+var temperatureF = (userInput - 32) / 1.8;
 
-var userInputDegrees = prompt ("Please enter F for Fahrenheit or C for Celsius");
-if(F){
-    prompt ("Please enter Temperature");
-}
+var userInputDegrees = prompt ("Please enter F for Fahrenheit to Celsius conversion \n or C for Celsius to Fahrenheit conversion");
+
+var userInput = prompt ("Please enter Temperature");
+console.log(temperatureF);
+    */
+
+
 
 
 
