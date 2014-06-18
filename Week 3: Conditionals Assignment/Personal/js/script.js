74331a7d3c863dd5e8c68749c9980974e30a706c
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-18 15:26:53 -0400

    added some comments and my given to personal script

--- Week 3: Conditionals Assignment/Personal/js/script.js
+++ Week 3: Conditionals Assignment/Personal/js/script.js
@@ -1,3 +1,13 @@
 /**
  * Created by Josh on 6/18/14.
  */
+//Joshua Ceres
+//June 18, 2014
+//Week 3 Graded Assignment:Conditionals
+
+//I am going to attempt to predict my date night with my wife for Sunday for out Anniversary.
+//We tend to eat and get tired afterwards. We always have intentions to go dancing and do something fun after we eat, but it seldom happens.
+//So I am going to start by using a sliding scale of 1-10 for energy level.
+//My Given energy level is 10 before we go to eat for both of us. (Whatever she is feeling I feel the same way.)
+
+var energyLevel = 10; //Given
