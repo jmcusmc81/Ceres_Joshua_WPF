8ff0d44c343ec5ff27981c4899a9ebd13895f450
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-19 17:19:29 -0400

    Added comments to industry assignment

--- Week 3: Conditionals Assignment/Industry/js/script.js
+++ Week 3: Conditionals Assignment/Industry/js/script.js
@@ -1,4 +1,12 @@
 /**
  * Created by Josh on 6/19/14.
  */
+/*
+Joshua Ceres
+June 19, 2014
+Week 3 Conditionals Assignment
+Industry
+ */
 
+//I am going to write a code if and else statement for checking to make sure all folders and files are present for initial setup of a web assignment
+//These folders are "css","images","js". The files are "index.html", and "script.js".
