f52ee1857fa8055a4ff391295a4b12437b6e5902
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-18 20:33:45 -0400

    Changed energy level to 5 to test out else condition

--- Week 3: Conditionals Assignment/Personal/js/script.js
+++ Week 3: Conditionals Assignment/Personal/js/script.js
@@ -10,11 +10,11 @@
 //So I am going to start by using a sliding scale of 1-10 for energy level.
 //My Given energy level is 10 before we go to eat for both of us. (Whatever she is feeling I feel the same way.)
 
-var energyLevel = 10; //Given
-var dancing = (energyLevel > 5); //Energy level needs to be higher then 5 for our night out after dinner.
+var energyLevel = 5; //Given
+var dancing = (energyLevel > 5); //Energy level needs to be higher then 5 for a night out after dinner.
 
 if(dancing){
-    console.log("Lets have some fun!");
+    console.log("Lets have some fun!"); //After a nice dinner its nice to go dancing with the wife.
 }else{
-    console.log("Lets go for a walk");
+    console.log("Lets go for a walk");//Sometimes after a nice dinner we are to tired and just want to go for a walk.
 }
