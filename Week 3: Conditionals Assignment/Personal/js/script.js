3cfbd90a9d1a2e3713ed2c0fd45df5a399fa0a05
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-18 21:21:57 -0400

    added else if statement wEnergyDrink and wrote out statement and tested the code.

--- Week 3: Conditionals Assignment/Personal/js/script.js
+++ Week 3: Conditionals Assignment/Personal/js/script.js
@@ -9,22 +9,31 @@
 //We tend to eat and get tired afterwards. We always have intentions to go dancing and do something fun after we eat, but it seldom happens.
 //So I am going to start by using a sliding scale of 1-10 for energy level.
 //My Given energy level is 10 before we go to eat for both of us. (Whatever she is feeling I feel the same way.)
-/*
-var energyLevel = 5; //Given
-var dancing = (energyLevel > 5); //Energy level needs to be higher then 5 for a night out after dinner.
 
-if(dancing){
+
+
+var energyLevel = 1; //Given
+var dancing = (energyLevel > 5); //Energy level needs to be higher then 5 for a night out after dinner.
+var wEnergyDrink = 2; //Boosts our energy levels.
+
+if(dancing) {
     console.log("Lets go dancing!"); //After a nice dinner its nice to go dancing with the wife.
+}else if (energyLevel + wEnergyDrink > 5){
+    console.log("Lets go dancing!"); //added else if Condition. Energy drink gives some boost to energy levels.
 }else{
     console.log("Lets go for a walk.");//Sometimes after a nice dinner we are to tired and just want to go for a walk.
 }
-    */
+//Changed Conditional Statement and added else if.
 
 //Ternary
 //Going to change the conditional statement to ternary operators.
 
+/*
 var energyLevel = 6;
 var dancing;
 
 dancing = (energyLevel > 5) ? "Lets go dancing!" : "Lets go for a walk.";
 console.log(dancing);
+    */
+//Ternary Operators worked out well for this statement.
+
