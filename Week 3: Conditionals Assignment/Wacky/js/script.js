c1465ab1159fb65a63257ee966d56a665cf66c8f
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-19 19:15:55 -0400

    initial setup

--- Week 3: Conditionals Assignment/Wacky/js/script.js
+++ Week 3: Conditionals Assignment/Wacky/js/script.js
@@ -1,3 +1,11 @@
 /**
  * Created by Josh on 6/19/14.
  */
+/*
+ Joshua Ceres
+ June 19, 2014
+ Week 3 Conditionals Assignment
+ Wacky
+ */
+
+//I am going to attempt some prompts for this part of the assignment.
