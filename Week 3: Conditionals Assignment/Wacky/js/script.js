1ee097521972439624559c1dc428430d9339c0e4
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-19 20:16:02 -0400

    Finally got my prompt to math out the coreect number

--- Week 3: Conditionals Assignment/Wacky/js/script.js
+++ Week 3: Conditionals Assignment/Wacky/js/script.js
@@ -16,12 +16,15 @@ console.log(d);
 var date = d.getFullYear();
 console.log(date);
 
-var userInput = prompt("Please enter your birth date \n MM/DD/Year");
+var userInput = prompt("Please enter your birth date \n Year only");
+
+var age = date - userInput;
+alert("Your age is " + age + " years old");
 
 
 
 
-if(userInput > date){
+if(age > ){
     console.log("Boy you are Old! You better get some adult diapers." + alert ("Boy you are Old! You better get some adult diapers.") );
 }else{
     console.log("You are young and still have some good times ahead!" + alert ("You are young and still have some good times ahead!") );
