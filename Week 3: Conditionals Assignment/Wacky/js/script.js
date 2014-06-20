6d54dd738a9706e39ff780a71b9830d98e43eabf
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-19 20:07:12 -0400

    Was trying to figure out how to get full year because I can not figure out how to get age from userInput through dates

--- Week 3: Conditionals Assignment/Wacky/js/script.js
+++ Week 3: Conditionals Assignment/Wacky/js/script.js
@@ -10,11 +10,16 @@
 
 //I am going to attempt some prompts for this part of the assignment.
 
-var today = new Date();
-console.log(today);
+var d = new Date();
+console.log(d);
+
+var date = d.getFullYear();
+console.log(date);
 
 var userInput = prompt("Please enter your birth date \n MM/DD/Year");
-var date = 01011979;
+
+
+
 
 if(userInput > date){
     console.log("Boy you are Old! You better get some adult diapers." + alert ("Boy you are Old! You better get some adult diapers.") );
