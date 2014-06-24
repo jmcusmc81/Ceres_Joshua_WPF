79ced6ba85aa4b3baf54b3e66f93fd836b263c6f
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-24 14:19:58 -0400

    following instructions

--- Screencast: Functions/js/script.js
+++ Screencast: Functions/js/script.js
@@ -3,4 +3,24 @@
  */
 //Functions - Basic Structures
 
+//Functions have same structure as condtionals
 
+// function functionName(are for parameters){
+// code the function runs
+// curly bracers are for blocks of code }
+
+
+//functions have names so we can refer to them later
+
+
+function outputMsg(){
+    console.log("Hello World");
+
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width* height;
+    console.log(area);
+}
