63d54b5ed176becc605ec59688bd92d78f995a98
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-25 12:20:30 -0400

    Problem 2 Stung

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -23,7 +23,21 @@ function calculateArea(w, h )  //w = 5 and h = 6 , parameters
 
 //Problem 1 Calculate the circumference of a circle.
 
-//c = 2x pi x r  //Circumference formula
+/*Circumference
+
+ Calculate the circumference of a circle.
+
+ Parameter(s) for function:
+    Radius of the circle
+
+ Return:
+    Circumference of the circle
+
+ Result to print to the console:
+    “The circumference of the circle is X”;
+    */
+
+ //c = 2x pi x r  //Circumference formula
 //Given will be Radius which 5
 
 var r = 5; //Given
@@ -35,3 +49,32 @@ function circumference(r, pi){
     return (r * pi) * 2;
 
 }
+//It should print out 31.4 for the answer
+
+
+
+
+/*
+Problem 2
+
+ Stung!
+
+ It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an animal in a function
+
+ Given:
+     Victim’s weight (in pounds)
+
+ Parameter(s) for function:
+    Victim’s weight (in pounds)
+
+ Return:
+    Number of Bee stings
+
+ Result to print to the console:
+    “It takes X bee stings to kill this animal.
+
+ */
+
+
+
+
