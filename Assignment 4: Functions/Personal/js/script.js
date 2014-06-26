be83f465cdf80ba5c61112806e31e3940b19da47
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-26 17:10:43 -0400

    added first var = cake

--- Assignment 4: Functions/Personal/js/script.js
+++ Assignment 4: Functions/Personal/js/script.js
@@ -6,3 +6,11 @@
 //Assignment 4:Functions
 
 //I am going to be doing a budget for my sons birthday party that is coming up next month.
+
+//Birthday Party
+
+//Named Function
+
+function birthdayParty1(){
+    var cake = 40;
+}
