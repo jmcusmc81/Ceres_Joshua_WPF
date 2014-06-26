b1e047607f7201be6436fe34dc2640f90d1e1920
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-26 17:09:21 -0400

    added comments and title

--- Assignment 4: Functions/Personal/js/script.js
+++ Assignment 4: Functions/Personal/js/script.js
@@ -1,3 +1,8 @@
 /**
  * Created by Josh on 6/26/14.
  */
+//Joshua Ceres
+//Wed June 26, 2014
+//Assignment 4:Functions
+
+//I am going to be doing a budget for my sons birthday party that is coming up next month.
