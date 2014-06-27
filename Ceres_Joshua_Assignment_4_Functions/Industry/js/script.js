ef9088aabade0dc11dda2a4f77a62b6b4d52074f
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-26 22:07:36 -0400

    updated comments

--- Ceres_Joshua_Assignment_4_Functions/Industry/js/script.js
+++ Ceres_Joshua_Assignment_4_Functions/Industry/js/script.js
@@ -9,20 +9,23 @@
 //I am having to login to every site I go on it seem like.
 //Its quite annoying honestly and I forget my passwords and usernames all the time.
 
-var userLogin = prompt("Please enter your Username");
+var userLogin = prompt("Please enter your Username");//User Input
 
-while(userLogin===""){
-    userLogin = prompt("Please enter you Username to continue");
+while(userLogin===""){  //Loop to validate prompt if nothing is entered
+    userLogin = prompt("Please enter you Username to continue"); //Re-prompt
 }
 
 console.log(userLogin);
 
-var userPassword = prompt("Please enter your password");
+var userPassword = prompt("Please enter your password"); //User Input Password
 
-while(userPassword===""){
+while(userPassword===""){ //Loop to validate
     userPassword = prompt("Please enter your password to continue");
 }
 console.log(userPassword);
 
-var securityInput = prompt("Please enter you security input number");
+var securityInput = prompt("Please enter you security input number"); //Security Input, numbers only.
 
+while (isNaN(securityInput)){ //Is not a number
+    securityInput = prompt("Please enter your security input number to continue"); //Validate and re-prompt if a number is not entered
+}
