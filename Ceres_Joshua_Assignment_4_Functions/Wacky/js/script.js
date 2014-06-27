3791e02e24ad39178305dffc64821dfce3ceb431
Joshua Ceres <jmc.usmc81@gmail.com>
2014-06-26 22:29:36 -0400

    comments and variables set

--- Ceres_Joshua_Assignment_4_Functions/Wacky/js/script.js
+++ Ceres_Joshua_Assignment_4_Functions/Wacky/js/script.js
@@ -6,3 +6,14 @@
 //Assignment 4:Functions
 
 //This is my wacky part of my Function assignment.
+//Going to make this simple ternary yes or no for my kids if they behave the entire day I will take them to the movies if they do not
+//Then they will be grounded the entire next day
+//They will get 2 points for doing chores, 2 points for listening, 2 points for not fighting or arguing, and 2 points for not repeating myself.
+//They need to get a 6 or higher to be able to go to the movies.
+
+
+var chores = 2;
+var listening = 2;
+var fighting = 2;
+var repeat = 2;
+
